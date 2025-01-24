4a7de71e0fb02c03c6ed24a3503ed0ecd613a01b
安东开煮了 <504134526@qq.com>
2025-01-25 00:07:11 +0800

    修复路由大小写问题

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-  pages: ["pages/index/index"],
+  pages: ["pages/Index/index"],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
