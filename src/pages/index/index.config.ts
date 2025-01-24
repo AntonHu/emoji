a259af70be48f58d86b54be48216b73bb1046aa7
安东开煮了 <504134526@qq.com>
2025-01-24 22:46:45 +0800

    修改导航栏配置

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,6 +1,3 @@
-import Taro from "@tarojs/taro";
-
 export default definePageConfig({
-  navigationBarTitleText:
-    Taro.getEnv() === Taro.ENV_TYPE.WEAPP ? "" : "Emoji查找神器",
+  navigationBarTitleText: "",
 });
